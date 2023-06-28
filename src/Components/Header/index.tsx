62375d1727795661a4d7209f8439018f35ab9684
shubham-js <shubbhamcomputer33@gmail.com>
2023-06-29 02:46:30 +0530

    Added Header HTML and CSS

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,4 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
+import "./index.css";
+import { headerConstants } from "../../Constants/index.js";
 export const Header = () => {
-  return <></>;
+  return (
+    <Fragment>
+      <div className="header">
+        <div className="logo_header box box_grow font25 fontW500 white_color">
+          {headerConstants.LOGO}
+        </div>
+        <div className="navbar">
+          <ul>
+            <li className="box font16">{headerConstants.HOME}</li>
+            <li className="box font16">{headerConstants.ABOUT}</li>
+            <li className="box font16">{headerConstants.WORK}</li>
+            <li className="box font16">{headerConstants.CONTACT}</li>
+          </ul>
+        </div>
+        <div className="logo_header box button font16 fontW500">
+          {headerConstants.LETS_TALK}
+        </div>
+      </div>
+    </Fragment>
+  );
 };
---
+++ src/Constants/index.js
@@ -0,0 +1,8 @@
+export const headerConstants = {
+  HOME: "Home",
+  ABOUT: "About",
+  WORK: "Work",
+  CONTACT: "Contact",
+  LETS_TALK: "Let's talk",
+  LOGO: "SJ",
+};
